cc78f60cecffc0e8894190af44b151d1f1180488
jayhan1109 <jayhan1109@gmail.com>
2020-12-16 12:47:49 +0900

    HomeScreen, ProductScreen

--- proshop/client/src/App.js
+++ proshop/client/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {Container} from "react-bootstrap";
+import {BrowserRouter, Route} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome To ProShop</h1>
-    </>
+    <BrowserRouter>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Route exact path='/' component={HomeScreen}/>
+          <Route path='/product/:id' component={ProductScreen}/>
+        </Container>
+      </main>
+      <Footer/>
+    </BrowserRouter>
   );
 };
 
---
+++ proshop/client/src/screens/HomeScreen.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from "react";
+import Product from "../components/Product";
+import {Col, Row} from "react-bootstrap";
+import Axios from "axios";
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const {data} = await Axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map(product => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product}/>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
