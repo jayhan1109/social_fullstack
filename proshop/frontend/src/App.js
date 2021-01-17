2b43c6467aca2456189898a0b381d7eaafa6719a
jayhan1109 <jayhan1109@gmail.com>
2021-01-17 15:45:00 -0800

    Created Model

--- proshop/frontend/src/App.js
+++ proshop/frontend/src/App.js
@@ -3,8 +3,8 @@ import {BrowserRouter, Route} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {Container} from "react-bootstrap";
-import HomeScreen from "./components/screens/HomeScreen";
-import ProductScreen from "./components/screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
